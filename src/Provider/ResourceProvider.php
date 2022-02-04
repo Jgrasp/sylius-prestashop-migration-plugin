af4dadd1cc92318a68a7902b574f518ca523cf5f
jgrasp <briaisq@hotmail.fr>
2022-02-04 11:32:06 +0100

    Force trait on provider

--- src/Provider/ResourceProvider.php
+++ src/Provider/ResourceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Jgrasp\PrestashopMigrationPlugin\Provider;
 
+use Exception;
+use Jgrasp\PrestashopMigrationPlugin\Entity\PrestashopTrait;
 use Jgrasp\PrestashopMigrationPlugin\Model\ModelInterface;
+use ReflectionClass;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Model\ResourceInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
@@ -21,6 +24,13 @@ final class ResourceProvider implements ResourceProviderInterface
 
     public function getResource(ModelInterface $model): ResourceInterface
     {
+        $reflection = new ReflectionClass($this->repository->getClassName());
+        $traits = $reflection->getTraitNames();
+
+        if (in_array(PrestashopTrait::class, $traits) === false) {
+            throw new Exception(sprintf("Entity %s should implement an instance of Trait %s", $this->repository->getClassName(), PrestashopTrait::class));
+        }
+
         $resource = $this->repository->findOneBy(['prestashopId' => $model->getId()]);
 
         if (!$resource) {
